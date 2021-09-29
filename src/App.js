cff30d5ce3667841764226485539cb47c6489648
Sharooq <iamsharooq@gmail.com>
2021-09-29 17:03:27 +0530

    authemntication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 /* src/App.js */
 import React, { useEffect, useState } from "react";
 import { API, graphqlOperation } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+
 import { createTodo } from "./graphql/mutations";
 import { listTodos } from "./graphql/queries";
 
@@ -100,4 +102,4 @@ const styles = {
   },
 };
 
-export default App;
+export default withAuthenticator(App);
